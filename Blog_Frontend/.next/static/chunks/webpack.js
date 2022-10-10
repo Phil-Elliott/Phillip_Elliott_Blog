92df6d267779cad4b9ae0b92df0de4a29a5e1b7c
Phil <phillippelliott@gmail.com>
2022-10-10 07:05:29 +0700

    fixed responsive header

--- Blog_Frontend/.next/static/chunks/webpack.js
+++ Blog_Frontend/.next/static/chunks/webpack.js
@@ -183,7 +183,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "cc8eff749ce087fd"; }
+/******/ 		__webpack_require__.h = function() { return "67571a67397e11bb"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
