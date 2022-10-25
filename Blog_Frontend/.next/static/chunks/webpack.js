84a7b6436658d5badf7ed62df350cbb40823dc62
Phil <phillippelliott@gmail.com>
2022-10-25 12:14:47 +0700

    try to fix server error

--- Blog_Frontend/.next/static/chunks/webpack.js
+++ Blog_Frontend/.next/static/chunks/webpack.js
@@ -183,7 +183,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "db01caeef462ef96"; }
+/******/ 		__webpack_require__.h = function() { return "3c1481ad4a6baf1f"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
