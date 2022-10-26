81dc063101338e7097e3e01b3e9a122e619d2b06
Phil <phillippelliott@gmail.com>
2022-10-26 10:53:12 +0700

    fixed featured title

--- Blog_Frontend/.next/static/chunks/webpack.js
+++ Blog_Frontend/.next/static/chunks/webpack.js
@@ -183,7 +183,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "10fc8c8a7de6cf21"; }
+/******/ 		__webpack_require__.h = function() { return "bc8354e37364ead0"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
